4955bf33319cfa1da79ec142916db6ee54c09575
omarsalim <83906211+Everezze@users.noreply.github.com>
2024-01-29 01:41:56 +0100

    email validation and its styles done

---
+++ script.js
@@ -0,0 +1,41 @@
+const button = document.querySelector("button");
+const input = document.querySelector("input");
+const errorMessage = document.querySelector(".label-state b");
+let validEmail = false;
+let submittedOnce = false;
+
+const regexEmail = /^\w+@\w+\.[a-z]{2,3}$/;
+
+input.addEventListener("input",function(){
+    if(input.value.match(regexEmail)){
+        validEmail = true;
+        if(submittedOnce){
+            input.classList.remove("active");
+            errorMessage.classList.remove("active");
+        }
+    }
+    else{
+        //using short-circuit logic to not re-set validEmail to false if it was
+        //already(maybe faster?)
+        if(!validEmail){
+            validEmail = false;
+        }
+        if(submittedOnce && !input.value == ""){
+            input.classList.add("active");
+            errorMessage.classList.add("active");
+        }
+        else{
+            input.classList.remove("active");
+            errorMessage.classList.remove("active");
+        }
+    }
+});
+
+button.addEventListener("click",function(event){
+    if(!validEmail){
+        input.classList.add("active");
+        errorMessage.classList.add("active");
+        event.preventDefault();
+    }
+    submittedOnce = true;
+});
